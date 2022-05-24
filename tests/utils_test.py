590d25213086c9e6cfbaf3c44ad99ad2dc67aecb
morph3 <melihkaanyildiz@gmail.com>
2022-05-24 18:34:49 +0300

    Update utils_test.py

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,19 +1,10 @@
 
 import sys
-sys.path.append(os.getcwd() + '/../src/')
-from Utils import *
-
 import os
 import unittest
 
-
-
-
-
-"""
-TODO: convert them into unit tests
-"""
-
+sys.path.append(os.getcwd() + '/../src/')
+from Utils import *
 
 class TestUtils(unittest.TestCase):
 
@@ -40,18 +31,5 @@ class TestUtils(unittest.TestCase):
         result = calculate_gini_index(data)
         self.assertEqual(result, 0)
 
-
 if __name__ == '__main__':
-    unittest.main()
-    """
-    TODO: convert below into unit tests
-    "For test purposes"
-
-    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    print(calculate_gini_index(data))
-    data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    print(calculate_gini_index(data))
-    data=[1]
-    print(calculate_gini_index(data))
-
-    """
+    unittest.main(verbosity=4)
